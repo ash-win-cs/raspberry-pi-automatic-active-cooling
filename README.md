fca8e60d87d51e1662ce11185b6bae2e67c34035
ashwin <ashwinc@gmail.com>
2021-04-01 16:35:20 +0530

    modified python script

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Raspberry-pi-automatic-active-cooling
-An automatic fan speed control for depending upon the temprature using python script.
+An automatic fan speed control for RPI4 depending upon the temprature using python script.
 
 
--- fanShutdownPWM.py
+++ fanShutdownPWM.py
@@ -9,7 +9,6 @@ import RPi.GPIO as GPIO
 
 
 fanPin = 17 # The pin ID, edit here to change it
-batterySensPin = 18
 
 desiredTemp = 45 # The maximum temperature in Celsius after which we trigger the fan
 
@@ -24,7 +23,7 @@ def Shutdown():
     sleep(100)
 def getCPUtemperature():
     res = os.popen('vcgencmd measure_temp').readline()
-    temp =(res.replace("temp=","").replace("'C\n",""))
+    temp = (res.replace("temp=","").replace("'C\n",""))
     #print("temp is {0}".format(temp)) #Uncomment here for testing
     return temp
 def fanOFF():
@@ -49,13 +48,6 @@ def handleFan():
     #print("actualTemp %4.2f TempDiff %4.2f pDiff %4.2f iDiff %4.2f fanSpeed %5d" % (actualTemp,diff,pDiff,iDiff,fanSpeed))
     myPWM.ChangeDutyCycle(fanSpeed)
     return()
-def handleBattery():
-    #print (GPIO.input(batterySensPin)) 
-    if GPIO.input(batterySensPin)==0:
-        ("Shutdown()")
-        sleep(5)
-        Shutdown()
-    return()		
 def setPin(mode): # A little redundant function but useful if you want to add logging
     GPIO.output(fanPin, mode)
     return()
@@ -65,12 +57,10 @@ try:
     GPIO.setup(fanPin, GPIO.OUT)
     myPWM=GPIO.PWM(fanPin,50)
     myPWM.start(50)
-    GPIO.setup(batterySensPin, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
     GPIO.setwarnings(False)
     fanOFF()
     while True:
         handleFan()
-        handleBattery()
         sleep(1) # Read the temperature every 5 sec, increase or decrease this limit if you want 
 except KeyboardInterrupt: # trap a CTRL+C keyboard interrupt 
     fanOFF()
